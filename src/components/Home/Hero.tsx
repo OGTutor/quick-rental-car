0e86dfdfee5edfc9c681a04b4d6615f32398438e
OGTutor <i.send.you.a.letter@gmail.com>
2024-02-05 20:39:50 +0200

    Updated a Hero component

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,59 +1,91 @@
+'use client';
+
 import Image from 'next/image';
 import { FC } from 'react';
+import CustomButton from '../CustomButton';
 
 const Hero: FC = () => {
+	const handleScroll = () => {};
+
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2">
-			<div>
-				<h2 className="text-[40px] md:text-[60px] font-bold text-white">
-					<span className="text-primaryColor animate-pulse">
+		<div
+			className="flex xl:flex-row flex-col
+			gap-5 relative z-0 max-w-[1440px] mx-auto"
+		>
+			<div className="flex-1 pt-36 sm:px-16 px-6">
+				<h1
+					className="2xl:text-[72px]
+					sm:text-[64px] text-[50px]
+					font-extrabold text-white"
+				>
+					<span
+						className="text-primaryColor
+						animate-pulse"
+					>
 						Premium{' '}
 					</span>
 					Car{' '}
-					<span className="text-primaryColor animate-pulse">
+					<span
+						className="text-primaryColor
+						animate-pulse"
+					>
 						Rental{' '}
 					</span>
 					in Your{' '}
-					<span className="text-primaryColor animate-pulse">
+					<span
+						className="text-primaryColor
+						animate-pulse"
+					>
 						Area{' '}
 					</span>
-				</h2>
-				<h2 className="text-[20px] text-gray-400 pr-20 mt-5">
-					<span className="text-primaryColor animate-pulse">
+				</h1>
+				<p
+					className="text-[27px] text-black-100
+					font-light mt-5"
+				>
+					<span
+						className="text-primaryColor
+						animate-pulse"
+					>
 						Book{' '}
 					</span>
 					the{' '}
-					<span className="text-primaryColor animate-pulse">
+					<span
+						className="text-primaryColor
+						animate-pulse"
+					>
 						selected{' '}
 					</span>
 					car effortlessly, Pay for driving only, Book the{' '}
-					<span className="text-primaryColor animate-pulse">
+					<span
+						className="text-primaryColor
+						animate-pulse"
+					>
 						Car Now{' '}
 					</span>
-				</h2>
-				<button
-					className="mt-5 hover:scale-105  hover:bg-primaryColor
-								px-4 cursor-pointer p-2
-								rounded-full hover:rounded-full
-								hover:text-black font-bold
-								text-primaryColor transition-all
-								shadow-sm shadow-white animate-pulse
-								hover:animate-none hover:shadow-primaryColor"
-				>
-					Explore Cars
-				</button>
+				</p>
+				<CustomButton title="Explore Cars" handleClick={handleScroll} />
 			</div>
-			<div>
-				<Image
-					src="/images/hero.png"
-					draggable={false}
-					alt="hero"
-					width={400}
-					height={400}
-					className="w-full object-cover align-middle animate-pulse"
-					unoptimized
-					quality={100}
-				/>
+			<div
+				className="xl:flex-[1.5] flex
+				justify-end items-end w-full
+				xl:h-screen"
+			>
+				<div
+					className="relative
+					xl:w-full w-[90%]
+					xl:h-full h-[590px] z-0"
+				>
+					<Image
+						src="/images/hero.png"
+						draggable={false}
+						alt="hero"
+						fill
+						className="object-contain animate-pulse"
+						unoptimized
+						quality={100}
+					/>
+				</div>
 			</div>
 		</div>
 	);

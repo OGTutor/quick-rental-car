319cee35c25cb21d99bdc22a72f67c2f1d55a455
OGTutor <i.send.you.a.letter@gmail.com>
2024-02-08 21:22:52 +0200

    Implemented calculateCarRent & fetchCars function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,34 @@ export function formatPrice(
 		maximumFractionDigits: 2,
 	}).format(numericPrice);
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+	const basePricePerDay = 50;
+	const mileageFactor = 0.1;
+	const ageFactor = 0.05;
+
+	const mileageRate = city_mpg * mileageFactor;
+	const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+	const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+	return rentalRatePerDay.toFixed(0);
+};
+
+export async function fetchCars() {
+	const headers = {
+		'X-RapidAPI-Key': 'f2fd0c5ce1mshc09f77ced2d41c0p1670a5jsn636c2f6fc4a2',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+	};
+
+	const response = await fetch(
+		'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+		{
+			headers: headers,
+		}
+	);
+
+	const result = await response.json();
+
+	return result;
+}

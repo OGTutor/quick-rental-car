5c1a8f44caaa2d7efc254339c50f311be20a5d8a
OGTutor <i.send.you.a.letter@gmail.com>
2024-02-15 20:55:52 +0200

    Some fixes & implemented searching in url`s page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,93 @@
-'use client';
-
-import CarsFiltersOption from '@/components/Home/CarsFiltersOption';
+import CarCard from '@/components/Home/CarCard';
+import CustomFilter from '@/components/Home/CustomFilter';
 import Hero from '@/components/Home/Hero';
-import SearchInput from '@/components/Home/SearchInput';
+import SearchBar from '@/components/Home/SearchBar';
+import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from '@/lib/utils';
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+	const allCars = await fetchCars({
+		manufacturer: searchParams.manufacturer || '',
+		year: searchParams.year || 2023,
+		fuel: searchParams.fuel || '',
+		limit: searchParams.limit || 10,
+		model: searchParams.model || '',
+	});
+
+	const isDataEmpty =
+		!Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
 	return (
-		<div className="overflow-hidden p-5 sm:px-10 md:px-28">
+		<>
 			<Hero />
-			<SearchInput />
-			<CarsFiltersOption />
-		</div>
+			<div
+				className="mt-12 sm:px-16
+				px-6 py-4 max-w-[1440px]
+				mx-auto"
+				id="discover"
+			>
+				<div
+					className="flex flex-col
+					items-start justify-start
+					gap-y-2.5 text-black-100"
+				>
+					<h1
+						className="text-4xl
+						font-extrabold"
+					>
+						Car Catalogue
+					</h1>
+					<p>Explore the cars you might like</p>
+				</div>
+				<div
+					className="mt-12 w-full
+					flex justify-between
+					items-center flex-wrap
+					gap-5"
+				>
+					<SearchBar />
+					<div
+						className="flex justify-start
+						flex-wrap items-center gap-2
+						mt-2"
+					>
+						<CustomFilter title="fuel" options={fuels} />
+						<CustomFilter
+							title="year"
+							options={yearsOfProduction}
+						/>
+					</div>
+				</div>
+				{!isDataEmpty ? (
+					<section>
+						<div
+							className="grid 2xl:grid-cols-4
+							xl:grid-cols-3 md:grid-cols-2
+							grid-cols-1 w-full gap-8 pt-14"
+						>
+							{allCars?.map((car, idx) => (
+								<CarCard key={idx} car={car} />
+							))}
+						</div>
+					</section>
+				) : (
+					<div
+						className="mt-16 flex
+						justify-center items-center
+						flex-col gap-2"
+					>
+						<h2
+							className="text-white
+							text-xl font-bold"
+						>
+							Oops, no results
+						</h2>
+						<p>{allCars?.message}</p>
+					</div>
+				)}
+			</div>
+			{/* <SearchInput />
+			<CarsFiltersOption /> */}
+		</>
 	);
 }

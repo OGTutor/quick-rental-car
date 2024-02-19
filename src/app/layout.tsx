d6f2bf83b576180b6820a8c81038fff57c5deb4b
OGTutor <i.send.you.a.letter@gmail.com>
2024-02-19 21:07:07 +0200

    Change theme of Clerk & added a ScrollToTopButton component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import Footer from '@/components/Footer';
+import MuiThemeProvider from '@/components/MuiThemeProvider';
 import Navbar from '@/components/Navbar';
+import ScrollToTopButton from '@/components/ScrollToTopButton';
 import {
 	ClerkProvider,
 	RedirectToSignIn,
 	SignedIn,
 	SignedOut,
 } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import type { Metadata } from 'next';
 import { Single_Day } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
@@ -25,34 +28,45 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider>
-			<html lang="en" className="h-full">
-				<body className={`${inter.className} relative`}>
-					<SignedIn>
-						<header className="w-full absolute z-10">
-							<Navbar />
-						</header>
-						<main className="overflow-hidden">
-							<div>
-								<ToastContainer />
-								{children}
-							</div>
-						</main>
-						<footer
-							className="w-full flex flex-col
-							text-white mt-5 border-t-[1px]
-							border-primaryColor shadow-sm
-							shadow-primaryColor
-							rounded-t-lg 3xl:rounded-t-full"
-						>
-							<Footer />
-						</footer>
-					</SignedIn>
-					<SignedOut>
-						<RedirectToSignIn />
-					</SignedOut>
-				</body>
-			</html>
+		<ClerkProvider
+			appearance={{
+				baseTheme: dark,
+				variables: {
+					colorPrimary: '#fed000',
+					colorTextOnPrimaryBackground: 'black',
+				},
+			}}
+		>
+			<MuiThemeProvider>
+				<html lang="en" className="h-full">
+					<body className={`${inter.className} relative`}>
+						<SignedIn>
+							<header className="w-full absolute z-10">
+								<Navbar />
+							</header>
+							<main className="overflow-hidden">
+								<div>
+									<ToastContainer />
+									{children}
+								</div>
+							</main>
+							<ScrollToTopButton />
+							<footer
+								className="w-full flex flex-col
+								text-white mt-5 border-t-[1px]
+								border-primaryColor shadow-sm
+								shadow-primaryColor
+								rounded-t-lg 3xl:rounded-t-full"
+							>
+								<Footer />
+							</footer>
+						</SignedIn>
+						<SignedOut>
+							<RedirectToSignIn />
+						</SignedOut>
+					</body>
+				</html>
+			</MuiThemeProvider>
 		</ClerkProvider>
 	);
 }

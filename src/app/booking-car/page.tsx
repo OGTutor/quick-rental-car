eeaefa09c1d644f6198fd2f18ef87a95be85aaf5
OGTutor <i.send.you.a.letter@gmail.com>
2024-02-23 22:17:32 +0200

    Implemented a basic BookingCar page

---
+++ src/app/booking-car/page.tsx
@@ -0,0 +1,56 @@
+import BookingCarCard from '@/components/BookingCar/BookingCarCard';
+import BookingInputLocation from '@/components/BookingCar/BookingInputLocation';
+import BookingInputNumber from '@/components/BookingCar/BookingInputNumber';
+import BookingMapBox from '@/components/BookingCar/BookingMapBox';
+import { fetchCars } from '@/lib/utils';
+import { FilterProps } from '@/types';
+import dynamic from 'next/dynamic';
+
+const BookingCalendar = dynamic(
+	() => import('@/components/BookingCar/BookingCalendar'),
+	{ ssr: false }
+);
+
+interface BookingCarProps {
+	searchParams: FilterProps;
+}
+
+export default async function BookingCar({ searchParams }: BookingCarProps) {
+	const [car] = await fetchCars({
+		manufacturer: searchParams.manufacturer || '',
+		year: searchParams.year || 2023,
+		fuel: searchParams.fuel || '',
+		limit: searchParams.limit || 10,
+		model: searchParams.model || '',
+	});
+
+	return (
+		<div
+			className="flex flex-col
+			gap-5 relative z-0 max-w-[1440px]
+			mx-auto items-center justify-center
+			md:justify-between"
+		>
+			<div
+				className="grid grid-cols-2 gap-5 mt-44 mb-9 sm:px-16 px-6
+				border border-black rounded-3xl
+				shadow-sm shadow-white text-center
+				py-20 hover:shadow-primaryColor
+				transition-all"
+			>
+				<div className="flex flex-col justify-start">
+					<BookingCarCard car={car} />
+					<BookingCalendar />
+					<BookingInputNumber />
+					<BookingInputLocation />
+				</div>
+				<div
+					className="flex flex-col justify-start shadow-sm shadow-white rounded-3xl hover:border-primaryColor transition-all overflow-hidden w-full h-[387px]
+					hover:scale-[1.02] hover:shadow-primaryColor hover:shadow-md"
+				>
+					<BookingMapBox />
+				</div>
+			</div>
+		</div>
+	);
+}
